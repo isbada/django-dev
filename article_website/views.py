e6a08b819a5e4f8ca22efb0c88150cf819f1b978
maydayfu <maydayfu@tencent.com>
2022-03-13 18:51:52 +0800

    ADD: course10: css美化页面+templates文件整理

--- article_website/views.py
+++ article_website/views.py
@@ -8,6 +8,13 @@ Copyright 2021 Tencent All rights reserved.
 '''
 
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
 
 def index(request):
     return HttpResponse("Hello, world!")
+
+
+def home(request):
+    context = {}
+    return render_to_response('home.html', context)
