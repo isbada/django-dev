2bb0fc1c2244e776c07ae55727d78aade5808ad0
maydayfu <maydayfu@tencent.com>
2022-03-14 15:31:53 +0800

    ADD: course12 bootstrap响应式布局

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from .models import Blog, BlogType
 def blog_list(request):
     context = {}
     context['blogs'] = Blog.objects.all()
+    context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blog_list.html', context)
 
 
@@ -20,4 +21,5 @@ def blogs_with_type(request, blog_type_pk):
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
     context['blogs'] = Blog.objects.filter(blog_type=blog_type)
     context['blog_type'] = blog_type
+    context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blogs_with_type.html', context)
